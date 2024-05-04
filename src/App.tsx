31f9d3c46d698f54ec24743c058800b281b52d3d
Jagdsh LK Chand <jagdsh.chand-ext@gucci.com>
2024-05-04 18:54:11 +0200

    Moving portfolio to portfolio.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import { lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './assets/styles/common.style.css'
+import "./assets/styles/common.style.css";
 
-
-const HomePage = lazy(() => import('./pages/home'));
-const Portfolio = lazy(() => import('./pages/portfolio'));
+const HomePage = lazy(() => import("./pages/home"));
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/portfolio"
+            element={<RedirectSite htmlPage={"/portfolio/index.html"} />}
+          />
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
+};
+
+interface RedirectSite {
+  htmlPage: string;
+}
+
+const RedirectSite = ({ htmlPage }: RedirectSite) => {
+  window.location.href = htmlPage;
+  return <></>;
 };
 
 export default App;
